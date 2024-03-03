f33c8c35f705b2d6c609cb8ccfd0f748c3e3a49a
Matheus Moreira <matheusmoreira2004@live.com>
2024-03-02 23:19:21 -0300

    feat: added auth screens

--- App.js
+++ App.js
@@ -6,17 +6,35 @@ import ChatScreen from "@/screens/ChatScreen";
 import CalendarScreen from "@/screens/CalendarScreen";
 import NotificationsScreen from "@/screens/NotificationsScreen";
 import UserScreen from "@/screens/UserScreen";
+import LoginScreen from "@/screens/auth/LoginScreen";
+import SignUpScreen from "@/screens/auth/SignUpScreen";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+
+  function AuthStack() {
+    return (
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Auth">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
         <Stack.Screen name="Calendar" component={CalendarScreen} />
         <Stack.Screen name="Notifications" component={NotificationsScreen} />
         <Stack.Screen name="User" component={UserScreen} />
+
+        <Stack.Screen
+          name="Auth"
+          component={AuthStack}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
