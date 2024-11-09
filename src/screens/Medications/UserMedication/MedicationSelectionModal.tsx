1f783ce6b8ede77ff978ab405534242e94c9cabd
Matheus Moreira <53829732+mathmore0000@users.noreply.github.com>
2024-11-09 00:12:21 -0300

    feat: add design medicationSelectionModal

--- src/screens/Medications/UserMedication/MedicationSelectionModal.tsx
+++ src/screens/Medications/UserMedication/MedicationSelectionModal.tsx
@@ -1,6 +1,6 @@
 // MedicationSelectionModal.js
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, TextInput, FlatList, ActivityIndicator, Modal } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, FlatList, ActivityIndicator, Modal, CheckBox } from "react-native";
 import api from "@/server/api";
 import { showErrorToast } from "@/utils/toast";
 import styles from "@/screens/_styles/medications";
@@ -8,7 +8,8 @@ import NewMedicationModal from "./Medication/NewMedicationModal";
 import UserMedicationModal from "./AddUserMedicationModal";
 import { medicationType } from "@/constants/interfaces/Entities";
 import { useTranslation } from "react-i18next";
-
+import Icon from "react-native-vector-icons/AntDesign";
+import IconEnty from "react-native-vector-icons/Entypo";
 const MedicationSelectionModal = ({ closeModal, userMedications, onUserMedicationCreated }) => {
   const { t } = useTranslation();
   const [medications, setMedications] = useState<medicationType[]>([]);
@@ -90,32 +91,56 @@ const MedicationSelectionModal = ({ closeModal, userMedications, onUserMedicatio
   };
 
   return (
-    <View style={styles.modalContainer}>
-      <Text style={styles.title}>{t("medications.selectMedication")}</Text>
+    <View className="flex-1 bg-white p-4">
+      <View className="flex flex-row items-center justify-between pb-4">
+        <Text className="font-bold text-xl">{t("medications.selectMedication")}</Text>
+        <TouchableOpacity
+          onPress={closeModal}
+        >
+          <Icon name="close" size={25} color="#000" />
+        </TouchableOpacity>
+      </View>
 
-      <TextInput
-        style={styles.searchInput}
-        placeholder={t("medications.searchMedication")}
-        value={searchText}
-        onChangeText={handleSearch}
-      />
+      <View className="flex-row items-center bg-gray-200 rounded-md px-2 py-1 mb-4">
+        <Icon name="search1" size={18} color="#000" />
+        <TextInput
+          className="flex-1 ml-2 text-base text-black"
+          placeholder={t("medications.searchMedication")}
+          value={searchText}
+          onChangeText={handleSearch}
+        />
+      </View>
+
+
 
       <FlatList
         data={filteredMedications}
         keyExtractor={(item) => item.id}
         ListEmptyComponent={<Text style={styles.emptyText}>{t("medications.noMedicationsFound")}</Text>}
         renderItem={({ item }) => (
-          <View style={item.id === selectedMedication.id ? styles.medicationItemSelected : styles.medicationItem}>
-            <Text style={styles.medicationName}>
-              {t("medications.medicationNameDosage", { name: item.name, dosage: item.dosage })}
-            </Text>
-            <TouchableOpacity
-              style={styles.selectButton}
-              onPress={() => handleSelect(item)}
-            >
-              <Text style={styles.selectButtonText}>{t("common.select")}</Text>
-            </TouchableOpacity>
-            <Text></Text>
+          <View className="flex flex-col px-2">
+            <View className="flex flex-row justify-between items-center py-3" >
+              <View className="flex flex-col">
+                <Text className="font-bold text-lg">
+                  {item.name}
+                </Text>
+                <Text className="">
+                  {t("medications.medicationDosage", { dosage: item.dosage })}
+                </Text>
+              </View>
+              <TouchableOpacity
+                className={`
+                w-7 h-7 border-2 border-gray-300 rounded-md items-center justify-center mr-2
+                ${item.id === selectedMedication.id && "bg-primary border-primary"}
+              `}
+                onPress={() => handleSelect(item)}
+              >
+                {item.id === selectedMedication.id && (
+                  <Icon name="check" size={16} color="#fff" />
+                )}
+              </TouchableOpacity>
+            </View>
+            <View className="border-b border-gray-300" />
           </View>
         )}
         onEndReached={loadMore}
@@ -138,18 +163,18 @@ const MedicationSelectionModal = ({ closeModal, userMedications, onUserMedicatio
           selectedMedication={selectedMedication}
         />
       </Modal>
+      <View className="border-t h-12 pt-2 border-gray-300 flex-row gap-2 w-full flex items-center" >
+        <TouchableOpacity onPress={openIsNewMedicationModal} className="w-1/2 gap-3 p-2 h-10 border border-primary rounded-md flex flex-row justify-center items-center">
+          <IconEnty name="add-to-list" size={20} color="#001F3F" />
+          <Text className="font-bold text-primary">{t("medications.addMedicationButton")}</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleNewUserMedicationModal}>
-        <Text style={styles.toggleText}>{t("medications.addToInventory")}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={handleNewUserMedicationModal} className="w-1/2 gap-1 p-2 h-10 bg-primary rounded-md flex flex-row justify-center items-center">
+          <Icon name="plus" size={20} color="#fff" />
+          <Text className="text-white font-bold">{t("medications.addToInventory")}</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={openIsNewMedicationModal}>
-        <Text style={styles.toggleText}>{t("medications.addMedicationButton")}</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.cancelButton} onPress={closeModal}>
-        <Text style={styles.cancelButtonText}>{t("common.cancel")}</Text>
-      </TouchableOpacity>
+      </View>
     </View>
   );
 };
